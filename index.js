8bca87c1b6f3c1fbc511b8002807e7d87c6f27e6
Jonathan <ruizjayy@icloud.com>
2021-07-19 11:14:27 -0500

    Updated CSS and generator functionality

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const fetch = require("node-fetch")
 
 
 app.use(express.static('public'))
 
-// app.get("/animequote", (req, res) => {
-//     fetch('https://animechan.vercel.app/api/random')
-//     .then(response => response.json())
-//     .then(quote => console.log(quote))
-// })
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
--- public/script.js
+++ public/script.js
@@ -1,4 +1,37 @@
 console.log("Running from inside script.js");
 
 
-const 
+const animeName = document.querySelector("#anime-name");
+const animeQuote = document.querySelector("#anime-quote");
+const animeCharacter = document.querySelector("#anime-character");
+const genBtn = document.querySelector("#quoteGenBtn");
+
+// genBtn.addEventListener("click", () => {
+//     getAnimeQuote();
+// })
+
+// function getAnimeQuote() {
+//     fetch('https://animechan.vercel.app/api/random')
+//     .then(response => response.json())
+//     // .then(quote => console.log(quote))
+//     .then(data => {
+//         animeName.innerHTML = data.anime
+//         animeQuote.innerHTML = data.quote
+//         animeCharacter.innerHTML = `-- ${data.character}`
+//     })
+// }
+
+genBtn.addEventListener("click", () => {
+    generateQuote();
+})
+
+function generateQuote() {
+    fetch('https://animechan.vercel.app/api/random')
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(data => {
+        animeName.textContent = data.anime;
+        animeQuote.textContent = data.quote;
+        animeCharacter.textContent = ` -- ${data.character}`;
+    })
+}
